1f26280f4db74ce7ff5dcba71b86aec6f2f47bbb
Piotr Sumara <piotrsumara@MacBook-Air.local>
2025-08-24 15:04:50 +0200

    Initial commit from Spring Initializr

---
+++ src/main/java/com/example/tesco/TescoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TescoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TescoApplication.class, args);
+	}
+
+}
